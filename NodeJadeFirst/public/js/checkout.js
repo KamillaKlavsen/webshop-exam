fed1a677dddb0b15dda4de22470e825d0c66ac51
KamillaKlavsen <kamillaklavsen@outlook.dk>
2021-12-17 21:24:17 +0100

    checkout stuff

---
+++ NodeJadeFirst/public/js/checkout.js
@@ -0,0 +1,68 @@
+let insertProduct = (product, quantity) => {
+    document.querySelector("table.shopping-cart > tbody").innerHTML += `
+    <tr>
+    <td>${product.Name}</td>
+    <td>${product.Price}</td>
+    <td><img src="/images/${product.ImageFile}" class="product-img"></td>
+    <td>${quantity}</td>
+    
+    <td>I alt: ${quantity*product.Price}</td>`;
+  };
+  {
+      /*
+      <td>${quantity}</td>
+  <td>I alt: ${quantity * product.Price}</td>
+  <td><div>
+  
+      <td>${product.Name}</td>
+  <td>${product.Price}</td>
+  <td><img src="/images/${product.ImageFile}" class="product-img"></td> */}
+  
+  let getProducts = () => {
+    if (localStorage.basket) {
+      let basketarr = JSON.parse(localStorage.basket);
+      basketarr.forEach((item) => {
+        fetch(`http://localhost:2000/product/${item.productId}`)
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json.data);
+            insertProduct(json.data, item.quantity);
+           
+  
+          });
+      });
+    }
+  };
+  let onordersubmit = (e) => {
+    e.preventDefault();
+    let order = {
+      CustomerName: e.target.CustomerName.value,
+      CustomerEmail: e.target.CustomerEmail.value,
+      Basketarr: localStorage.basket,
+    };
+    let reqData = {
+      method: "post",
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(order),
+      mode: "cors",
+    };
+    let req = new Request(`http://localhost:2000/order`, reqData);
+  
+    let clearBasket = localStorage.clear();
+
+    fetch(req)
+      .then((response) => response.json())
+      .then((json) => { //json
+        //   console.log(json.data);
+        console.log(req);
+        alert('Tak for dit køb!');
+    });
+    clearBasket(); //virker måske
+  };
+  
+  document.addEventListener("DOMContentLoaded", (e) => {
+    getProducts();
+    document.forms.order.addEventListener("submit", onordersubmit);
+    
+  });
+  
